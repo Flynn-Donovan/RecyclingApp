9df767cf7d3271d2fe3f333f21ecc3af15e489a7
Midpoint Programmer <midpoint.programmer@gmail.com>
2026-03-07 17:23:59 -0700

    add pages folder.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,8 @@
 import './styles/shared.css'
 import './styles/main.css'
 function App() {
- 
-
   return (
     <>
-      <body>
-
-      <div className="container">
-        <div className='section'>
-          
-        </div>
-      </div>
-
-      </body>
     </>
   )
 }
